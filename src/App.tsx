dd53621aabc0f66a8acad4d6f556e09dac8e0acd
신지섭 <ddslk75@naver.com>
2024-05-23 01:55:47 +0900

    add title & navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    const navBar = document.querySelector(".nav-container");
+    if (navBar instanceof HTMLElement) {
+      if (toggle) {
+        navBar.style.left = "0";
+      } else {
+        navBar.style.left = "-10rem";
+      }
+    }
+  }, [toggle]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='header-title'>
+          TITLE
+        </div>
+        <ul className='header-right-container'>
+          <li>Sign In</li>
+          <li>Sign Up</li>
+        </ul>
       </header>
+
+      <nav>
+        <button className='nav-open-btn' onClick={() => {
+          setToggle(true);
+        }}>
+          Open
+        </button>
+        <ul className='nav-container'>
+          <li onClick={() => {
+            setToggle(false);
+          }}>Close</li>
+          <li>주문하기</li>
+          <li>메뉴</li>
+          <li>게시판</li>
+        </ul>
+      </nav>
     </div>
   );
 }
